1a28a12cf37c0eb3afc3b9c4f0e31dabfc63671a
Pushkar Singh <stoazest@gmail.com>
2022-01-29 17:40:56 +0530

    added essential key

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.post('/signup', (req, res) => {
 
   const postData = JSON.stringify(data);
 
-  fetch('https://usX.api.mailchimp.com/3.0/lists/<YOUR_AUDIENCE_ID>', {
+  fetch('https://us20.api.mailchimp.com/3.0/lists/ef6a19445b', {
     method: 'POST',
     headers: {
-      Authorization: 'auth <YOUR_API_KEY>'
+      Authorization: 'auth b687427b92834ae62b6ff8700b80fdf7-us20'
     },
     body: postData
   })
@@ -50,6 +50,6 @@ app.post('/signup', (req, res) => {
     .catch(err => console.log(err))
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
